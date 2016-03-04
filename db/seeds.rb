191ba70597731198e3569fbbe47198f01f6233e5
nataraja <nataraja.b@kvpcorp.com>
2016-03-05 02:46:03 +0530

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,9 @@ r1 = Role.create({name: "Customer", description: "Can read items"})
 r2 = Role.create({name: "Seller", description: "Can read and create items. Can update and destroy own items"})
 r3 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
 
-u1 = User.create({name: "raj", email: "raj@example.com", password: "12345678", password_confirmation: "12345678", role_id: r1.id})
+u1 = User.create({name: "raj", email: "raj@gmail.com.com", password: "12345678", password_confirmation: "12345678", role_id: r1.id})
 u2 = User.create({name: "raj", email: "raja@example.com", password: "12345678", password_confirmation: "12345678", role_id: r2.id})
 u3 = User.create({name: "raj", email: "admin@example.com", password: "12345678", password_confirmation: "12345678", role_id: r3.id})
+u4 = User.create({name: "raj", phone: "8792226597", address:"Kasaragod", email: "nataraja.b@kvpcorp.com", password: "12345678", password_confirmation: "12345678", role_id: r1.id})
 
 
