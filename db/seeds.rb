52481f0ce70ce84c2feefc853bb63009dea65ffe
nataraja <nataraja.b@kvpcorp.com>
2016-03-05 06:12:44 +0530

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ r1 = Role.find_or_create_by({name: "Customer", description: "Can read items"})
 r2 = Role.find_or_create_by({name: "Seller", description: "Can read and find_or_create_by items. Can update and destroy own items"})
 r3 = Role.find_or_create_by({name: "Admin", description: "Can perform any CRUD operation on any resource"})
 
-# u1 = User.find_or_create_by({name: "raj", email: "raj@gmail.com", password: "12345678", password_confirmation: "12345678", role_id: r1.id})
-# u2 = User.find_or_create_by({name: "raj", email: "raja@example.com", password: "12345678", password_confirmation: "12345678", role_id: r2.id})
-u3 = User.find_or_create_by({name: "raj", landmark:"Near forum Mall", phone:"7795179882", address:"Adugodi", city:"Bangalore", state:"Karnataka", country:"India", email: "admin@example.com", password: "12345678", password_confirmation: "12345678", role_id: r3.id})
+u1 = User.find_or_create_by({name: "raj", email: "raj@gmail.com", password: "12345678", password_confirmation: "12345678", role_id: r1.id})
+u2 = User.find_or_create_by({name: "raj", email: "raja@example.com", password: "12345678", password_confirmation: "12345678", role_id: r2.id})
+u3 = User.create({name: "raj", landmark:"Near forum Mall", phone:"7795179882", address:"Adugodi", city:"Bangalore", state:"Karnataka", country:"India", email: "admin@example.com", password: "12345678", password_confirmation: "12345678", role_id: r3.id}) unless User.find_by_email("admin@example.com")
 # u4 = User.find_or_create_by({name: "raj", phone: "8792226597", address:"Kasaragod", email: "nataraja.b@kvpcorp.com", password: "12345678", password_confirmation: "12345678", role_id: r1.id})
 
 
