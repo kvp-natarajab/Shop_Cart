4b393c5f6daa5818f49627026653442497b0d21a
nataraja <nataraja.b@kvpcorp.com>
2016-03-08 19:02:24 +0530

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
-
+uri = URI.parse(ENV["REDIS_URL"] || "redis://localhost:6379/" )
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
