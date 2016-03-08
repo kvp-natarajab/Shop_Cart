89342e8b38ead41e64aba6eb2f4ba90c91ada367
nataraja <nataraja.b@kvpcorp.com>
2016-03-08 05:43:00 +0530

    submit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 
 
-r1 = Role.create({name: "Customer", description: "Can read items"}) 
-r2 = Role.create({name: "Seller", description: "Can read and find_or_create_by items. Can update and destroy own items"}) 
-r3 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
-u1 = User.create({name: "raj", landmark:"Near forum Mall", phone:"7795179882", address:"Adugodi", city:"Bangalore", state:"Karnataka", country:"India", pincode:"671552", email: "natarajavcet@gmail.com", password: "12345678", password_confirmation: "12345678",confirmed_at:Time.now, role_id:3})
-u2 = User.create({name: "raj", landmark:"Near forum Mall", phone:"7795179881", address:"Adugodi", city:"Bangalore", state:"Karnataka", country:"India", pincode:"671552", email: "raja4u9@gmail.com", password: "12345678", password_confirmation: "12345678",confirmed_at:Time.now, role_id:2})
- 
+# r1 = Role.create({name: "Customer", description: "Can read items"}) 
+# r2 = Role.create({name: "Seller", description: "Can read and find_or_create_by items. Can update and destroy own items"}) 
+# r3 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
+# u1 = User.create({name: "raj", landmark:"Near forum Mall", phone:"7795179882", address:"Adugodi", city:"Bangalore", state:"Karnataka", country:"India", pincode:"671552", email: "natarajavcet@gmail.com", password: "12345678", password_confirmation: "12345678",confirmed_at:Time.now, role_id:3})
+# u2 = User.create({name: "raj", landmark:"Near forum Mall", phone:"7795179881", address:"Adugodi", city:"Bangalore", state:"Karnataka", country:"India", pincode:"671552", email: "raja4u9@gmail.com", password: "12345678", password_confirmation: "12345678",confirmed_at:Time.now, role_id:2})
+ u2 = User.create({name: "raj", landmark:"Near forum Mall", phone:"7795279881", address:"Adugodi", city:"Bangalore", state:"Karnataka", country:"India", pincode:"671552", email: "customer@gmail.com", password: "12345678", password_confirmation: "12345678",confirmed_at:Time.now, role_id:1})
  
 
 
