acdae2bdaef975bee23c687b9456e448c0d78926
nataraja <nataraja.b@kvpcorp.com>
2016-03-08 05:54:29 +0530

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 # r3 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
 # u1 = User.create({name: "raj", landmark:"Near forum Mall", phone:"7795179882", address:"Adugodi", city:"Bangalore", state:"Karnataka", country:"India", pincode:"671552", email: "natarajavcet@gmail.com", password: "12345678", password_confirmation: "12345678",confirmed_at:Time.now, role_id:3})
 # u2 = User.create({name: "raj", landmark:"Near forum Mall", phone:"7795179881", address:"Adugodi", city:"Bangalore", state:"Karnataka", country:"India", pincode:"671552", email: "raja4u9@gmail.com", password: "12345678", password_confirmation: "12345678",confirmed_at:Time.now, role_id:2})
- u2 = User.create({name: "raj", landmark:"Near forum Mall", phone:"7795279881", address:"Adugodi", city:"Bangalore", state:"Karnataka", country:"India", pincode:"671552", email: "customer@gmail.com", password: "12345678", password_confirmation: "12345678",confirmed_at:Time.now, role_id:1})
+ u2 = User.create({name: "raj", landmark:"Near forum Mall", phone:"7795275881", address:"Adugodi", city:"Bangalore", state:"Karnataka", country:"India", pincode:"671552", email: "customer@gmail.com", password: "12345678", password_confirmation: "12345678",confirmed_at:Time.now, role_id:1})
  
 
 
